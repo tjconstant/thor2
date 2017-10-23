30013d49defcfe0f2f853894183f1d7e79e1c61c
Tom Constant <t.j.constant@gmail.com>
2017-10-23 22:59:59 +0100

    adds decent tests

--- tests/testthat/test-filter_class.R
+++ tests/testthat/test-filter_class.R
@@ -1,13 +1,29 @@
 context("filter_class")
 
-test_that("filters return correct transmission", {
+test_that("Class returns name", {
   thorlabs_filter_import("FEL0550")
-  FEL0550$name
-  FEL0550$plot()
-  FEL0550$data
-  FEL0550$Transmission(200:700)
+  expect_equal(FEL0550$name, "FEL0550")
+})
 
+test_that("Filter data returned is correct length", {
+  thorlabs_filter_import("FEL0550")
+  expect_length(FEL0550$data[,1], 2401)
+})
 
+test_that("Filter returns correct transmission", {
+  thorlabs_filter_import("FEL0550")
+  expect_equal(round(FEL0550$Transmission(555),3), round(0.8538235,3))
+})
+
+test_that("Filter returns error on intialization when filter doesn't exist",{
   expect_error(thorlabs_filter_import("FEL590"))
+})
+
+test_that("All Filters can be loaded", {
+  expect_error({
+    for(f in unique(filters$filter)){
+      thorlabs_filter_import(f)
+    }
+  }, NA)
 })
 
